4e9f8fa4a50f08eb4a680b6913912b1ad1b55a26
NathanGregoar <he201832@students.ephec.be>
2023-05-17 23:42:22 +0200

    Update index.php

--- reseau externe/web/sites/php/index.php
+++ reseau externe/web/sites/php/index.php
@@ -7,6 +7,7 @@
         <form method="POST" action="?"> 
             <input type="search" placeholder="nom du jouet" name="nomDuProduit">
             <input type="search" placeholder="prix du jouet" name="prixDuProduit">
+            <input type="search" placeholder="quantité du jouet" name="quantiteDuProduit">
             <button type="submit"> Ajouter le nouveau jouet ! </button>
         </form>
 
@@ -18,18 +19,32 @@
         $sql = 'SELECT * FROM Products';
 
         if ($result = $conn->query($sql)){
+            echo "<table><th>Nos Produits</th><th>Prix</th><th>Quantité</th>";
             while ($data = $result->fetch_assoc()) {
-                $produits[] = $data;
-            }
-            echo "<table><th>Nos Produits</th>";
-            foreach ($produits as $prod) {
                 echo "<tr><td>";
-                echo $prod['Product'];
+                echo $data['Product'];
                 echo "</td><td>";
-                echo $prod['Quantity'];
+                echo $data['Price'];
+                echo "</td><td>";
+                echo $data['Quantity'];
                 echo "</td><tr>";
             }
             echo "</table>";
+        }
+
+        // Handle form submission
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $nomProduit = $_POST['nomDuProduit'];
+            $prixDuProduit = $_POST['prixDuProduit'];
+            $quantiteDuProduit = $_POST['quantiteDuProduit'];
+
+            // Insert new toy into the database
+            $insertSql = "INSERT INTO Products (Product, Quantity, Price) VALUES ('$nomProduit', $quantiteDuProduit, $prixDuProduit)";
+            if ($conn->query($insertSql) === TRUE) {
+                echo "<p>Jouet ajouté avec succès !</p>";
+            } else {
+                echo "Erreur lors de l'ajout du jouet : " . $conn->error;
+            }
         }
     ?>
 
